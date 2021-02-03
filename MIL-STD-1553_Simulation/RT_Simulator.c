5481892504a174bce4d712ee332326eb6344acfd
ellem95 <66348743+ellem95@users.noreply.github.com>
2021-02-02 21:53:57 -0700

    Create RT_Simulator.c

---
+++ MIL-STD-1553_Simulation/RT_Simulator.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "MIL-STD-1553_Library.h"
+
+
+#define SOURCE_PORT 2001
+#define DESTINATION_PORT 2000
+
+
+int main()
+{
+    initialize_socket(SOURCE_PORT)
+    return 0;
+}
