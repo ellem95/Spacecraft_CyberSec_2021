6df659d25a85d0f19246ca73420dbc8a8f376cb7
Eloise Morris <66348743+ellem95@users.noreply.github.com>
2021-02-09 01:04:26 -0700

    Update BC_Simulator.c

--- MIL-STD-1553_Simulation/BC_Simulator.c
+++ MIL-STD-1553_Simulation/BC_Simulator.c
@@ -1,14 +1,16 @@
 #include "MIL-STD-1553_Library.c"
 
-#define SOURCE_PORT 2000
-#define DESTINATION_PORT 2001
-#define RT_ADDRESS 0x01
-#define USER_CLASS RT_CLASS
+#define SOURCE_PORT 9997
+#define DESTINATION_PORT 9998
+#define RT_ADDRESS 0x00
+#define USER_CLASS 1
 
 
 int main()
 {
     initialize_library(SOURCE_PORT, DESTINATION_PORT, RT_ADDRESS, USER_CLASS);
+    send_data_to_rt(01, 10, "SOME MESSAGE");
+    request_data_from_rt(1, 7, 5);
     while (1);
     return 0;
     
