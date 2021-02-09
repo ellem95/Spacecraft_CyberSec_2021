445616a011b52aa7e8e2cfeafcbafe293fa229b7
Eloise Morris <66348743+ellem95@users.noreply.github.com>
2021-02-09 01:02:05 -0700

    Update BC_Simulator.c

--- MIL-STD-1553_Simulation/BC_Simulator.c
+++ MIL-STD-1553_Simulation/BC_Simulator.c
@@ -1,21 +1,15 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sys/types.h> 
-#include <sys/socket.h> 
-#include <netinet/in.h> 
-#include <unistd.h>
-#include <arpa/inet.h>
-#include "MIL-STD-1553_Library.h"
+#include "MIL-STD-1553_Library.c"
 
 #define SOURCE_PORT 2000
 #define DESTINATION_PORT 2001
+#define RT_ADDRESS 0x01
+#define USER_CLASS RT_CLASS
 
 
 int main()
 {
-    intialize_socket(SOURCE_PORT);
-    send_data_to_rt(01, 10, "SOME MESSAGE");
-    request_data_from_rt(1, 7, 5);
+    initialize_library(SOURCE_PORT, DESTINATION_PORT, RT_ADDRESS, USER_CLASS);
+    while (1);
     return 0;
+    
 }
