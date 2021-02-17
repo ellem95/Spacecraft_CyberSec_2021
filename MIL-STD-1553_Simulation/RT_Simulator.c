0defeb083c544960235f6af360bb01e17d6507ea
Eloise Morris <66348743+ellem95@users.noreply.github.com>
2021-02-16 23:40:42 -0700

    Update RT_Simulator.c

--- MIL-STD-1553_Simulation/RT_Simulator.c
+++ MIL-STD-1553_Simulation/RT_Simulator.c
@@ -2,9 +2,21 @@
 
 #define SOURCE_PORT 2001
 #define DESTINATION_PORT 2000
-#define RT_ADDRESS 0x01
+#define RT_ADDRESS FLIGHT_SOFTWARE //This RT is representing flight software so uses the flight software RT address
 #define USER_CLASS RT_CLASS
 
+/* =====================================================
+
+   This program is used to initialize a bus controller
+   on the machine simulating the BC. The port the BC is
+   listening on (SOURCE_PORT) and the port the BC should
+   send data to (DESTINATION_PORT) can be defined in the
+   variables above. All initialization is done within
+   the MIL-STD-1553_Library. To add more remote terminals,
+   define their function and address at the top of the 
+   1553 Library code.
+
+   ===================================================== */
 
 int main()
 {
